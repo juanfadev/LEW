05f3f205fb18d27ae42eae0facbd9ac15c4107b0
juanfpo96 <juanfpo96@gmail.com>
2018-09-29 20:15:37 +0200

    Added changes of feed

--- XML/Tema 5/XML_Feed/xml_sports_feed.py
+++ XML/Tema 5/XML_Feed/xml_sports_feed.py
@@ -12,7 +12,7 @@ def main():
 
     doc = xmltodict.parse(input_xml, encoding='utf-8', dict_constructor = dict)
     mydata = createNewXML(doc)
-    myfile = open("segunda_division.xml", "w")  
+    myfile = open("segunda_division.xml", "wb")  
     myfile.write(mydata)  
 
 def createNewXML(input_xml):
@@ -21,27 +21,28 @@ def createNewXML(input_xml):
     feed = ET.Element('feed')
     feed.set('title', root['title'])
     feed.set('link', root['link'])
+    feed.set('published', root['pubDate'])
+    feed.set('lang', root['language'])
+    copyright = ET.SubElement(feed, 'copyright')
+    copyright.text = root['copyright']
+    image = ET.SubElement(feed,'image')
+    image.set('title', root['image']['title'])
+    imURL = ET.SubElement(image, 'url')
+    imURL.text = root['image']['url']
+    imLink = ET.SubElement(image, 'link')
+    imLink.text = root['image']['link']
     items = ET.SubElement(feed, 'items')
     for i in root['item']:
         item = ET.SubElement(items, 'item')
         item.set('title', i['title'])
-        
+        item.set('description', i['description'])
+        item.set('creator', i['dc:creator'])
+        link =  ET.SubElement(item, 'link')
+        item.text = i['link']
         categories = ET.SubElement(item, 'categories')
-        
         for c in i['category']:
             category = ET.SubElement(categories, 'category')
             category.text = i['category']
-
-
-
-        
-    item1 = ET.SubElement(items, 'item')
-    item2 = ET.SubElement(items, 'item')
-    
-    item1.set('name','item1')  
-    item2.set('name','item2')  
-    item1.text = 'item1abc'  
-    item2.text = 'item2abc'
 
     # create a new XML file with the results
     mydata = ET.tostring(feed)  
